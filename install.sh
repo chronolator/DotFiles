fb689913283e9ddc8d227cfcb7a41e580e700f14
chronolator <mikestuzi@gmail.com>
2022-11-08 17:22:24 -0600

    Updated lua and bash script

--- install.sh
+++ install.sh
@@ -13,7 +13,9 @@ TMUX="tmux"
 NVIM="nvim"
 DISTRO=$(cat /etc/os-release | grep -io "^id=.*" | awk -F'=' '{print $2}' | sed 's/\"//g')
 RHEL=("centos" "rhel" "fedora" "rocky")
+RHEL_PKGS="neovim xsel zsh tmux exa fzf"
 DEB=("ubuntu" "debian" "linuxmint" "kali")
+DEB_PKGS="neovim xsel zsh tmux exa fzf"
 
 # Ensure that arguments are specified
 if [[ -z "$1" ]]; then
@@ -30,9 +32,9 @@ if [[ "$PART" == 1 ]]; then
     info 0 "Guessing OS distro and installing dependencies"
     echo ""
     if [[ $DISTRO == ${DEB[0]} ]] || [[ $DISTRO == ${DEB[1]} ]] || [[ $DISTRO == ${DEB[2]} ]] || [[ $DISTRO == ${DEB[3]} ]]; then
-        allcheck 0 "Attempting to install dependencies" apt install nvim xsel zsh tmux exa fzf -y
+        allcheck 0 "Attempting to install dependencies" apt install $DEB_PKGS -y
     elif [[ $DISTRO == ${RHEL[0]} ]] || [[ $DISTRO == ${RHEL[1]} ]] || [[ $DISTRO == ${RHEL[2]} ]] || [[ $DISTRO == ${RHEL[3]} ]]; then
-        allcheck 0 "Attempting to install dependencies" yum install nvim xsel zsh tmux exa fzf -y
+        allcheck 0 "Attempting to install dependencies" yum install $RHEL_PKGS -y
     else
         info 1 "Unable to guess the OS distro"
         echo ""
@@ -40,25 +42,30 @@ if [[ "$PART" == 1 ]]; then
     fi
 
     # Create .config if it does not already exist
-    allcheck 0 "Creating ~/.config/ folder" mkdir -p ~/.config/
+    allcheck 0 "Creating ~/.config/ folder" mkdir -p $HOME_CONFIG/
 
     # Install zsh dotfiles
-    allcheck 0 "" cp -pvf $CONFIG/
+    allcheck 0 "Installing zsh dotfiles" cp -pvf $CONFIG/$ZSH $HOME_CONFIG/
+    allcheck 0 "Creating .zshrc symlink" ln -sv $HOME_CONFIG/$ZSH/.zshrc $HOME/.zshrc
     
     # Install tmux dotfiles
+    allcheck 0 "Installing tmux dotfiles" cp -pvf $CONFIG/$TMUX $HOME_CONFIG/
+    allcheck 0 "Creating .tmux.conf symlink" ln -sv $HOME_CONFIG/$TMUX/.tmux.conf $HOME/.tmux.conf
     
     # Install LunarVim
     # https://github.com/LunarVim/Neovim-from-scratch
     git clone https://github.com/LunarVim/Neovim-from-scratch.git ~/.config/nvim
     
-    
-    # Replace LunarVim with nvim dotfiles
-    
-    
+    # Replace LunarVim with neovim dotfiles
+    allcheck 0 "Installing nvim dotfiles" cp -pvf $CONFIG/$NVIM $HOME_CONFIG/
+
     # Launch nvim and wait for plugins to be installed
-    
-    
+    echo -e "$be${c[13]}Now manually launch nvim and wait for the plugins to finish installing, which can be seen at the bottom status bar$ee"
+fi
+
+# PART 2
+if [[ "$PART" == 2 ]]; then
     # Edit nvim theme
-    
-    ln -s $HOME/.config/zsh/.zshrc $HOME/.zshrc
-    mkdir -p $HOME/.config/zsh/plugins/zsh-antigen/ && curl -L git.io/antigen > $HOME/.config/zsh/plugins/antigen/antigen.zsh
+    allcheck 0 "Modifying the default neovim theme to be transparent" sed -i 's/transparent\ =\ true/transparent\ =\ false/g' $HOME/.local/share/nvim/site/pack/packer/start/tokyonight.nvim/lua/tokyonight/config.lua
+fi
+
