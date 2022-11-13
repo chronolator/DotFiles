258c467978dcf77d44e2b0edcf76faade1fb7985
Chronolator <mikestuzi@gmail.com>
2022-11-12 19:10:50 -0600

    Updated to allow a normal user to install, but not for root user profile yet

--- install.sh
+++ install.sh
@@ -7,27 +7,32 @@ if [[ ! -d "$DIR" ]]; then DIR="$PWD"; fi
 
 # Options
 CONFIG="./.config"
-HOME_CONFIG="$HOME/.config"
+PART="$1"
+HOME_USER="$(getent passwd $SUDO_USER | cut -d: -f6)"
+HOME_CONFIG="$HOME_USER/.config"
 ZSH="zsh"
 TMUX="tmux"
-NVIM="nvim"
+NVIM="lvim"
 DISTRO=$(cat /etc/os-release | grep -io "^id=.*" | awk -F'=' '{print $2}' | sed 's/\"//g')
 RHEL=("centos" "rhel" "fedora" "rocky")
 RHEL_PKGS="neovim xsel zsh tmux exa fzf"
 DEB=("ubuntu" "debian" "linuxmint" "kali")
 DEB_PKGS="neovim xsel zsh tmux exa fzf"
 
+# Ensure user is running as root
+rootcheck
+
 # Ensure that arguments are specified
-if [[ -z "$1" ]]; then
-    echo -e "$be${c[11]}Part 1 is for installing zsh, tmux, and LunarVim dotfiles, and Part 2 is for after LunarVim has installed its plugins."
-    echo -e "$be${c[10]}$0$ee $be${c[9]} <Part 1|2>$ee"
-    echo -e "Example: $0 1"
-    exit
-fi
-echo ""
+#if [[ -z "$1" ]]; then
+#    echo -e "$be${c[11]}Part 1 is for installing zsh, tmux, and LunarVim dotfiles, and Part 2 is for after LunarVim has installed its plugins."
+#    echo -e "$be${c[10]}$0$ee $be${c[9]} <Part 1|2>$ee"
+#    echo -e "Example: $0 1"
+#    exit
+#fi
+#echo ""
 
 # If PART 1, then install zsh, tmux, and LunarVim, if PART 2, then edit LunarVim plugins
-if [[ "$PART" == 1 ]]; then
+#if [[ "$PART" == 1 ]]; then
     # Ensure that dependencies are installed
     info 0 "Guessing OS distro and installing dependencies"
     echo ""
@@ -45,27 +50,38 @@ if [[ "$PART" == 1 ]]; then
     allcheck 0 "Creating ~/.config/ folder" mkdir -p $HOME_CONFIG/
 
     # Install zsh dotfiles
-    allcheck 0 "Installing zsh dotfiles" cp -pvf $CONFIG/$ZSH $HOME_CONFIG/
-    allcheck 0 "Creating .zshrc symlink" ln -sv $HOME_CONFIG/$ZSH/.zshrc $HOME/.zshrc
-    
+    allcheck 0 "Installing zsh dotfiles" cp -pvrf $CONFIG/$ZSH $HOME_CONFIG/
+    allcheck 0 "Creating .zshrc symlink" ln -sv $HOME_CONFIG/$ZSH/.zshrc $HOME_USER/.zshrc
+
     # Install tmux dotfiles
-    allcheck 0 "Installing tmux dotfiles" cp -pvf $CONFIG/$TMUX $HOME_CONFIG/
-    allcheck 0 "Creating .tmux.conf symlink" ln -sv $HOME_CONFIG/$TMUX/.tmux.conf $HOME/.tmux.conf
-    
+    allcheck 0 "Installing tmux dotfiles" cp -pvrf $CONFIG/$TMUX $HOME_CONFIG/
+    allcheck 0 "Creating .tmux.conf symlink" ln -sv $HOME_CONFIG/$TMUX/.tmux.conf $HOME_USER/.tmux.conf
+
     # Install LunarVim
     # https://github.com/LunarVim/Neovim-from-scratch
-    git clone https://github.com/LunarVim/Neovim-from-scratch.git ~/.config/nvim
-    
+    #git clone https://github.com/LunarVim/Neovim-from-scratch.git $HOME_CONFIG/$NVIM
+    su $SUDO_USER -c "LV_BRANCH='release-1.2/neovim-0.8' bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh)"
+
     # Replace LunarVim with neovim dotfiles
-    allcheck 0 "Installing nvim dotfiles" cp -pvf $CONFIG/$NVIM $HOME_CONFIG/
+    #allcheck 0 "Installing lvim dotfiles" cp -pvf $CONFIG/$NVIM/lua/user/* $HOME_CONFIG/$NVIM/lua/user/
+
+    # Set ownwership to the user
+    allcheck 0 "Changing ownwership for $HOME_CONFIG/$ZSH" chown -R $SUDO_USER:$SUDO_USER $HOME_CONFIG/$ZSH
+    allcheck 0 "Changing ownwership for $HOME_CONFIG/$TMUX" chown -R $SUDO_USER:$SUDO_USER $HOME_CONFIG/$TMUX
+    allcheck 0 "Changing ownwership for $HOME_CONFIG/$NVIM" chown -R $SUDO_USER:$SUDO_USER $HOME_CONFIG/$NVIM
+
+    # Edit nvim theme
+    allcheck 0 "Modifying the default neovim theme to be transparent" sed -i 's/transparent\ =\ false/transparent\ =\ true/g' "$HOME_USER/.local/share/lunarvim/site/pack/packer/start/tokyonight.nvim/lua/tokyonight/config.lua"
+    allcheck 0 "Modifying the default neovim colorscheme to be tokyonight" sed -i 's/lvim.colorscheme\ =\ "lunar"/lvim.colorscheme\ =\ "tokyonight"/g' "$HOME_CONFIG/$NVIM/config.lua"
 
     # Launch nvim and wait for plugins to be installed
     echo -e "$be${c[13]}Now manually launch nvim and wait for the plugins to finish installing, which can be seen at the bottom status bar$ee"
-fi
+#fi
 
 # PART 2
-if [[ "$PART" == 2 ]]; then
+#if [[ "$PART" == 2 ]]; then
     # Edit nvim theme
-    allcheck 0 "Modifying the default neovim theme to be transparent" sed -i 's/transparent\ =\ true/transparent\ =\ false/g' $HOME/.local/share/nvim/site/pack/packer/start/tokyonight.nvim/lua/tokyonight/config.lua
-fi
+    #allcheck 0 "Modifying the default neovim theme to be transparent" sed -i 's/transparent\ =\ false/transparent\ =\ true/g' "$HOME_USER/.local/share/lunarvim/site/pack/packer/start/tokyonight.nvim/lua/tokyonight/config.lua"
+    #allcheck 0 "Modifying the default neovim colorscheme to be tokyonight" sed -i 's/lvim.colorscheme\ =\ "lunar"/lvim.colorscheme\ =\ "tokyonight"/g' "$HOME_CONFIG/$NVIM/config.lua"
+#fi
 
