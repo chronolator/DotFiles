2e204b4cd4adc12078523d2d3fa3e7007c506c43
chronolator <mikestuzi@gmail.com>
2022-11-16 11:24:52 -0600

    Updated installer and added zsh prompt for root

--- install.sh
+++ install.sh
@@ -7,7 +7,8 @@ if [[ ! -d "$DIR" ]]; then DIR="$PWD"; fi
 
 # Options
 CONFIG="./.config"
-PART="$1"
+CUSER="$1"
+CID="$(getent passwd $CUSER | cut -d: -f3)"
 HOME_USER="$(getent passwd $SUDO_USER | cut -d: -f6)"
 HOME_CONFIG="$HOME_USER/.config"
 ZSH="zsh"
@@ -23,65 +24,96 @@ DEB_PKGS="neovim xsel zsh tmux exa fzf"
 rootcheck
 
 # Ensure that arguments are specified
-#if [[ -z "$1" ]]; then
-#    echo -e "$be${c[11]}Part 1 is for installing zsh, tmux, and LunarVim dotfiles, and Part 2 is for after LunarVim has installed its plugins."
-#    echo -e "$be${c[10]}$0$ee $be${c[9]} <Part 1|2>$ee"
-#    echo -e "Example: $0 1"
-#    exit
-#fi
-#echo ""
+if [[ -z "$1" ]]; then
+    echo -e "$be${c[10]}$0$ee $be${c[9]} <USER>$ee"
+    echo -e "Example: $0 root"
+    echo -e "Example: $0 bob"
+    exit
+fi
 
-# If PART 1, then install zsh, tmux, and LunarVim, if PART 2, then edit LunarVim plugins
-#if [[ "$PART" == 1 ]]; then
-    # Ensure that dependencies are installed
-    info 0 "Guessing OS distro and installing dependencies"
+# Check if user does not exist
+if ! id "$CUSER" &> /dev/null; then
+    echo -e "$be${c[9]}User does not exist$ee"
+    exit
+fi
+
+# Ensure that dependencies are installed
+info 0 "Guessing OS distro and installing dependencies"
+echo ""
+if [[ $DISTRO == ${DEB[0]} ]] || [[ $DISTRO == ${DEB[1]} ]] || [[ $DISTRO == ${DEB[2]} ]] || [[ $DISTRO == ${DEB[3]} ]]; then
+    allcheck 0 "Attempting to install dependencies" apt install $DEB_PKGS -y
+elif [[ $DISTRO == ${RHEL[0]} ]] || [[ $DISTRO == ${RHEL[1]} ]] || [[ $DISTRO == ${RHEL[2]} ]] || [[ $DISTRO == ${RHEL[3]} ]]; then
+    allcheck 0 "Attempting to install dependencies" yum install $RHEL_PKGS -y
+else
+    info 1 "Unable to guess the OS distro"
     echo ""
-    if [[ $DISTRO == ${DEB[0]} ]] || [[ $DISTRO == ${DEB[1]} ]] || [[ $DISTRO == ${DEB[2]} ]] || [[ $DISTRO == ${DEB[3]} ]]; then
-        allcheck 0 "Attempting to install dependencies" apt install $DEB_PKGS -y
-    elif [[ $DISTRO == ${RHEL[0]} ]] || [[ $DISTRO == ${RHEL[1]} ]] || [[ $DISTRO == ${RHEL[2]} ]] || [[ $DISTRO == ${RHEL[3]} ]]; then
-        allcheck 0 "Attempting to install dependencies" yum install $RHEL_PKGS -y
-    else
-        info 1 "Unable to guess the OS distro"
-        echo ""
-        exit
-    fi
+    exit
+fi
 
+# If user exists and is not root
+if id "$CUSER" &> /dev/null && [[ "$CID" -ne 0 ]]; then
     # Create .config if it does not already exist
-    allcheck 0 "Creating ~/.config/ folder" mkdir -p $HOME_CONFIG/
+    allcheck 0 "Creating ~/.config/ folder" mkdir -pv $HOME_CONFIG/
 
     # Install zsh dotfiles
     allcheck 0 "Installing zsh dotfiles" cp -pvrf $CONFIG/$ZSH $HOME_CONFIG/
-    allcheck 0 "Creating .zshrc symlink" ln -sv $HOME_CONFIG/$ZSH/.zshrc $HOME_USER/.zshrc
+    allcheck 0 "Removing zsh-prompt-root for a normal one" rm -fv $HOME_CONFIG/$ZSH/.zsh-prompt-root
+    allcheck 0 "Creating .zshrc symlink" ln -svf $HOME_CONFIG/$ZSH/.zshrc $HOME_USER/.zshrc
 
     # Install tmux dotfiles
     allcheck 0 "Installing tmux dotfiles" cp -pvrf $CONFIG/$TMUX $HOME_CONFIG/
-    allcheck 0 "Creating .tmux.conf symlink" ln -sv $HOME_CONFIG/$TMUX/.tmux.conf $HOME_USER/.tmux.conf
+    allcheck 0 "Creating .tmux.conf symlink" ln -svf $HOME_CONFIG/$TMUX/.tmux.conf $HOME_USER/.tmux.conf
 
     # Install LunarVim
     # https://github.com/LunarVim/Neovim-from-scratch
     #git clone https://github.com/LunarVim/Neovim-from-scratch.git $HOME_CONFIG/$NVIM
+    info 0 "Installing LunarVim"
     su $SUDO_USER -c "LV_BRANCH='release-1.2/neovim-0.8' bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh)"
-
-    # Replace LunarVim with neovim dotfiles
-    #allcheck 0 "Installing lvim dotfiles" cp -pvf $CONFIG/$NVIM/lua/user/* $HOME_CONFIG/$NVIM/lua/user/
+    ifcheck "installing LunarVim"
 
     # Set ownwership to the user
-    allcheck 0 "Changing ownwership for $HOME_CONFIG/$ZSH" chown -R $SUDO_USER:$SUDO_USER $HOME_CONFIG/$ZSH
-    allcheck 0 "Changing ownwership for $HOME_CONFIG/$TMUX" chown -R $SUDO_USER:$SUDO_USER $HOME_CONFIG/$TMUX
-    allcheck 0 "Changing ownwership for $HOME_CONFIG/$NVIM" chown -R $SUDO_USER:$SUDO_USER $HOME_CONFIG/$NVIM
+    allcheck 0 "Changing ownwership for $HOME_CONFIG/$ZSH" chown -Rv $SUDO_USER:$SUDO_USER $HOME_CONFIG/$ZSH
+    allcheck 0 "Changing ownwership for $HOME_CONFIG/$TMUX" chown -Rv $SUDO_USER:$SUDO_USER $HOME_CONFIG/$TMUX
+    allcheck 0 "Changing ownwership for $HOME_CONFIG/$NVIM" chown -Rv $SUDO_USER:$SUDO_USER $HOME_CONFIG/$NVIM
 
-    # Edit nvim theme
-    allcheck 0 "Modifying the default neovim theme to be transparent" sed -i 's/transparent\ =\ false/transparent\ =\ true/g' "$HOME_USER/.local/share/lunarvim/site/pack/packer/start/tokyonight.nvim/lua/tokyonight/config.lua"
-    allcheck 0 "Modifying the default neovim colorscheme to be tokyonight" sed -i 's/lvim.colorscheme\ =\ "lunar"/lvim.colorscheme\ =\ "tokyonight"/g' "$HOME_CONFIG/$NVIM/config.lua"
+# If user exists and is root/has an id of 0 (Yes, I know copying code is bad practice, whatever I will fix this later)
+elif id "$CUSER" &> /dev/null && [[ "$CID" -eq 0 ]]; then
+    # Set HOME_USER to the root user
+    HOME_USER=$(getent passwd $USER | cut -d: -f6)
+    HOME_CONFIG="$HOME_USER/.config"
 
-    # Launch nvim and wait for plugins to be installed
-    echo -e "$be${c[13]}Now manually launch nvim and wait for the plugins to finish installing, which can be seen at the bottom status bar$ee"
-#fi
+    # Create .config if it does not already exist
+    allcheck 0 "Creating ~/.config/ folder" mkdir -pv $HOME_CONFIG/
 
-# PART 2
-#if [[ "$PART" == 2 ]]; then
-    # Edit nvim theme
-    #allcheck 0 "Modifying the default neovim theme to be transparent" sed -i 's/transparent\ =\ false/transparent\ =\ true/g' "$HOME_USER/.local/share/lunarvim/site/pack/packer/start/tokyonight.nvim/lua/tokyonight/config.lua"
-    #allcheck 0 "Modifying the default neovim colorscheme to be tokyonight" sed -i 's/lvim.colorscheme\ =\ "lunar"/lvim.colorscheme\ =\ "tokyonight"/g' "$HOME_CONFIG/$NVIM/config.lua"
-#fi
+    # Install zsh dotfiles
+    allcheck 0 "Installing zsh dotfiles" cp -pvrf $CONFIG/$ZSH $HOME_CONFIG/
+    allcheck 0 "Installing zsh root prompt" mv -vf $HOME_CONFIG/$ZSH/zsh-prompt-root $HOME_CONFIG/$ZSH/zsh-prompt
+    allcheck 0 "Creating .zshrc symlink" ln -svf $HOME_CONFIG/$ZSH/.zshrc $HOME_USER/.zshrc
 
+    # Install tmux dotfiles
+    allcheck 0 "Installing tmux dotfiles" cp -pvrf $CONFIG/$TMUX $HOME_CONFIG/
+    allcheck 0 "Creating .tmux.conf symlink" ln -svf $HOME_CONFIG/$TMUX/.tmux.conf $HOME_USER/.tmux.conf
+
+    # Install LunarVim
+    # https://github.com/LunarVim/Neovim-from-scratch
+    #git clone https://github.com/LunarVim/Neovim-from-scratch.git $HOME_CONFIG/$NVIM
+    info 0 "Installing LunarVim"
+    LV_BRANCH='release-1.2/neovim-0.8' bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh)
+    ifcheck "installing LunarVim"
+
+    # Set ownwership to the user
+    allcheck 0 "Changing ownwership for $HOME_CONFIG/$ZSH" chown -Rv $USER:$USER $HOME_CONFIG/$ZSH
+    allcheck 0 "Changing ownwership for $HOME_CONFIG/$TMUX" chown -Rv $USER:$USER $HOME_CONFIG/$TMUX
+    allcheck 0 "Changing ownwership for $HOME_CONFIG/$NVIM" chown -Rv $USER:$USER $HOME_CONFIG/$NVIM
+fi
+
+# Change default login shell for user
+ZSH_PATH="$(which zsh)"
+allcheck 0 "Changing the default login shell to zsh" sed -ri "\%$CUSER%s%:/bin.*|:/usr/bin.*%:${ZSH_PATH}%g" /etc/passwd
+
+# Edit nvim theme
+allcheck 0 "Modifying the default neovim theme to be transparent" sed -i 's/transparent\ =\ false/transparent\ =\ true/g' "$HOME_USER/.local/share/lunarvim/site/pack/packer/start/tokyonight.nvim/lua/tokyonight/config.lua"
+allcheck 0 "Modifying the default neovim colorscheme to be tokyonight" sed -i 's/lvim.colorscheme\ =\ "lunar"/lvim.colorscheme\ =\ "tokyonight"/g' "$HOME_CONFIG/$NVIM/config.lua"
+
+# Launch nvim and wait for plugins to be installed
+echo -e "$be${c[13]}Now manually launch lvim and wait for the plugins to finish installing, which can be seen at the bottom status bar$ee"
